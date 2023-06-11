16f184fd4eebe8866b352b24d2a8edb1e4b4c987
david wekesa <dweke12@gmail.com>
2023-06-11 09:37:25 +0300

    commit

--- src/Components/Functions.js
+++ src/Components/Functions.js
@@ -1,23 +1,23 @@
 import CryptoJS from "crypto-js";
 export const carrt = [];
 
-const key = CryptoJS.enc.Hex.parse(process.env.REACT_APP_FireBase_ApiKey);
-const iv = CryptoJS.enc.Hex.parse(process.env.REACT_APP_FireBase_App_Id);
-export const enCrypt = (plaintext) => {
-  // const plaintext = 'This is my secret message';
+// const key = CryptoJS.enc.Hex.parse(process.env.REACT_APP_FireBase_ApiKey);
+// const iv = CryptoJS.enc.Hex.parse(process.env.REACT_APP_FireBase_App_Id);
+// export const enCrypt = (plaintext) => {
+//   // const plaintext = 'This is my secret message';
 
-  const encrypted = CryptoJS.AES.encrypt(plaintext, key, { iv: iv }).toString();
+//   const encrypted = CryptoJS.AES.encrypt(plaintext, key, { iv: iv }).toString();
 
-  return encrypted;
-};
-// Decrypting a message
-export const deCrypt = (encrypted) => {
-  const decrypted = CryptoJS.AES.decrypt(encrypted, key, { iv: iv }).toString(
-    CryptoJS.enc.Utf8
-  );
+//   return encrypted;
+// };
+// // Decrypting a message
+// export const deCrypt = (encrypted) => {
+//   const decrypted = CryptoJS.AES.decrypt(encrypted, key, { iv: iv }).toString(
+//     CryptoJS.enc.Utf8
+//   );
 
-  return decrypted;
-};
+//   return decrypted;
+// };
 
 //delete element from array
 const DeleteFromArray = (cartItems, id, cartItem) => {
