b437e4df55f8c0aac1305b6064a874c4a507e67b
david wekesa <dweke12@gmail.com>
2024-03-07 12:16:20 +0300

    blog

--- src/Components/Blog/ShareBlogPost.jsx
+++ src/Components/Blog/ShareBlogPost.jsx
@@ -8,11 +8,25 @@ function ShareBlogPost({ blogHeading }) {
   const copiedToClip = () => toast.success("Copied to clipboard");
   useEffect(() => {
     // Get the current URL
-    const currentUrl = window.location.href;
-    const blogUrl = `${currentUrl}?q=${blogHeading.toString()}`;
-
-    setCopyValue(blogUrl);
-    console.log("Current URL:", blogUrl);
+    // const currentUrl = window.location.href;
+    // console.log("copied link current ",currentUrl)
+    // const blogUrl = ${currentUrl}?q=${blogHeading.toString()};
+    // console.log("copied link blogUrl ",blogUrl)
+    // setCopyValue(blogUrl);
+    // console.log("Current URL:", blogUrl);
+    const urlParams = new URLSearchParams(window.location.search);
+    const qParamValue = urlParams.get("q");
+    const desiredValue = blogHeading;
+    if (qParamValue === desiredValue) {
+      const currentUrl = window.location.href;
+      setCopyValue(currentUrl);
+    } else {
+      urlParams.set("q", desiredValue);
+      const newUrl = `${window.location.origin}${
+        window.location.pathname
+      }?${urlParams.toString()}`;
+      setCopyValue(newUrl);
+    }
   }, []);
 
   useEffect(() => {
